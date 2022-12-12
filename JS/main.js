6fc7b9d6f7a8352195cd49e8042400dd7d7c87ea
nemo2109 <114691858+nemo2109@users.noreply.github.com>
2022-12-12 13:41:46 -0600

    Colores del body modificados

--- JS/main.js
+++ JS/main.js
@@ -6,6 +6,8 @@ const main = document.getElementById('main');
 const form = document.getElementById('form');
 const seccionPrincipal = document.getElementById('contenido-1');
 
+const contenedorBotones = document.getElementById('contenedor-btns');
+
 // Inputs de la forma
 const sentimiento = document.getElementById('sentimiento');
 
@@ -18,35 +20,57 @@ form.addEventListener('submit', (e) => {
 
 // Crear un evento global 
 main.addEventListener('click', (e) => {
-  if(e.target.classList.contains('feliz')) {
+  if (e.target.classList.contains('feliz-btn')) {
     // Agregar clases a body
     body.removeAttribute('class');
     body.classList.add('feliz');
 
     // Agregar la clase a la seccion
+    seccionPrincipal.classList.remove('enojado-s', 'triste-s', 'tranquilo-s');
     seccionPrincipal.classList.add('feliz-s');
+
+    // Llamar funcion para agregar texto
+    insertarEstadoDeAnimo('Feliz');
   }
-  if(e.target.classList.contains('triste')) {
+
+  if(e.target.classList.contains('triste-btn')) {
     body.removeAttribute('class');
     body.classList.add('triste');
 
     // Agregar la clase a la seccion
-    seccionPrincipal.classList.add('triste-s')
+    seccionPrincipal.classList.remove('enojado-s', 'tranquilo-s', 'feliz-s');
+    seccionPrincipal.classList.add('triste-s');
+
+    // Llamar funcion para agregar texto
+    insertarEstadoDeAnimo('Triste')
   }
 
-  if(e.target.classList.contains ('tranquilo')) {
+  if(e.target.classList.contains ('tranquilo-btn')) {
     body.removeAttribute('class');
     body.classList.add('tranquilo');
 
     // Agregar la clase a la seccion
+    seccionPrincipal.classList.remove('enojado-s', 'feliz-s', 'triste-s');
     seccionPrincipal.classList.add('tranquilo-s')
+
+    // Llamar funcion para agregar texto
+    insertarEstadoDeAnimo('Tranquilo')
   }
 
-  if(e.target.classList.contains ('enojado')) {
+  if(e.target.classList.contains ('enojado-btn')) {
     body.removeAttribute('class');
     body.classList.add('enojado');
 
     // Agregar la clase a la seccion
+    seccionPrincipal.classList.remove('triste-s', 'feliz-s', 'tranquilo-s')
     seccionPrincipal.classList.add('enojado-s');
+
+    // Llamar funcion para agregar texto
+    insertarEstadoDeAnimo('Enojo')
   }
 });
+
+function insertarEstadoDeAnimo (animo) {
+  const texto = document.getElementById('estado-animo');
+  texto.textContent = animo;
+}
